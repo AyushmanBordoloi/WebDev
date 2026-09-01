f6d78d13e1525c7770017dd4d08aac59710b6e4c
AyushmanBordoloi <ayushmanbordoloi02@gmail.com>
2026-09-01 20:24:49 +0530

    Implement chalk library in nodeJS

---
+++ NodeJs/index.mjs
@@ -0,0 +1,4 @@
+import chalk from 'chalk';
+console.log(chalk.blue('Hello, world!'));
+console.log(chalk.green.bold('This is a success message.'));
+console.log(chalk.red.underline('This is an error message.'));
