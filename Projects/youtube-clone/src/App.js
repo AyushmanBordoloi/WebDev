d40748cc8aff8ed79573e9ebd86cd49446297dc2
AyushmanBordoloi <ayushmanbordoloi02@gmail.com>
2026-09-26 23:03:00 +0530

    Add setup for new full stack youtube-clone project

---
+++ Projects/youtube-clone/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    //BEM class naming convention (lowercase, hyphen-separated)
+    <div className="app">
+      <h1>YouTube Clone</h1>
+    </div>
+  );
+}
+
+export default App;
